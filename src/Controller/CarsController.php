7f38f2d311b39e6ac22f993217bfce4b18f08615
baonqgcd210561 <baonqgcd210561@fpt.edu.vn>
2023-10-10 10:21:06 +0700

    update

---
+++ src/Controller/CarsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Car;
+use App\Form\CarType;
+use App\Repository\CarRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class CarsController extends AbstractController
+{
+    /**
+     * @Route("/cars", name="car_list")
+     */
+    public function index(CarRepository $carRepository): Response
+    {
+        $cars = $carRepository->findAll();
+
+        return $this->render('cars/index.html.twig', [
+            'cars' => $cars,
+        ]);
+    }
+    
+    /**
+     * @Route("/cars/add", name="add_car")
+     */
+    public function addcar(Request $request): Response
+    {
+        $newcar = new Car();
+        $form = $this->createForm(CarType::class, $newcar);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newcar);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('car_list');
+        }
+
+        return $this->render('cars/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+     /**
+     * @Route("/cars/{id}", name="view_car")
+     */
+    public function viewCar($id, CarRepository $carRepository): Response
+    {
+        $car = $carRepository->find($id);
+
+        if (!$car) {
+            throw $this->createNotFoundException('car not found');
+        }
+
+        return $this->render('cars/view.html.twig', [
+            'car' => $car,
+        ]);
+    }
+    /**
+     * @Route("/cars/{id}/delete", name="delete_car")
+     */
+    public function deleteCar($id, CarRepository $CarRepository): Response
+    {
+        $car = $CarRepository->find($id);
+
+        if (!$car) {
+            throw $this->createNotFoundException('car not found');
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($car);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('car_list');
+    }
+        /**
+     * @Route("/cars/{id}/edit", name="edit_car")
+     */
+    public function editCar($id, CarRepository $CarRepository, Request $request): Response
+    {
+        $car = $CarRepository->find($id);
+
+        if (!$car) {
+            throw $this->createNotFoundException('car not found');
+        }
+
+        $form = $this->createForm(CarType::class, $car);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('car_list');
+        }
+
+        return $this->render('cars/edit.html.twig', [
+            'car' => $car,
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Entity/Car.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarRepository::class)
+ */
+class Car
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $namecar;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNamecar(): ?string
+    {
+        return $this->namecar;
+    }
+
+    public function setNamecar(string $namecar): self
+    {
+        $this->namecar = $namecar;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
